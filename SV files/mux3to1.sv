11effb46bba1c3fcd4e0249949a9409a5c16c8af
Anirudh N S <pes2202101128@pesu.pes.edu>
2024-07-29 15:36:16 +0530

    Create mux3to1.sv

---
+++ SV files/mux3to1.sv
@@ -0,0 +1,16 @@
+module mux3to1
+    #(parameter WIDTH = 9)
+    (input logic [WIDTH-1:0] d0, d1, d2,
+     input logic [1:0] s,
+     output logic [WIDTH-1:0] y);
+
+always_comb
+    begin
+        case (s)
+            2'b00: y = d0;
+            2'b01: y = d1;
+            2'b10: y = d2;
+            default: y = d0;
+        endcase
+    end
+endmodule
