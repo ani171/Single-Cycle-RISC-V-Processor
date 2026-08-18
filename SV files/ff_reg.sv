90f5f9efda1c9568da713ec6dc31067a9b66730a
Anirudh <anirudh04027@gmail.com>
2026-08-18 16:41:20 +0530

    Update ff_reg.sv

--- SV files/ff_reg.sv
+++ SV files/ff_reg.sv
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 
 module ff_reg #(
-    parameter WIDTH = 8
-) (
-    input logic clk,             // Clock input
-    input logic reset,           // Synchronous reset input
-    input logic [WIDTH-1:0] d,   // Data input
-    output logic [WIDTH-1:0] q   // Data output
+    parameter WIDTH = 32
+)(
+    input logic clk,
+    input logic reset,
+    input logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
 );
-
-always_ff @(posedge clk) begin
-    if (reset) 
-        q <= 0;
-    else 
-        q <= d;
-end
+    // Synchronous reset
+    always_ff @(posedge clk) begin
+        if (reset)
+            q <= '0;
+        else
+            q <= d;
+    end
 
 endmodule
