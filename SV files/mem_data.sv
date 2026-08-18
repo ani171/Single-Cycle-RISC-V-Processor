55c7263a6378e9fa035463599b3483d9003e9ee5
Anirudh <anirudh04027@gmail.com>
2026-08-18 16:46:40 +0530

    Update mem_data.sv

--- SV files/mem_data.sv
+++ SV files/mem_data.sv
@@ -3,28 +3,37 @@
 module mem_data #(
     parameter DM_ADDRESS = 9,
     parameter DATA_W = 32
-    )(
+)(
     input logic clk,
-    input logic MemRead, // comes from control unit
-    input logic MemWrite, // Comes from control unit
-    input logic [DM_ADDRESS-1:0] a, // Read / Write address - 9 LSB bits of the ALU output
-    input logic [DATA_W-1:0] wd, // Write Data
-    output logic [DATA_W-1:0] rd // Read Data
-    );
-    
+    input logic MemRead,
+    input logic MemWrite,
+    input logic [DM_ADDRESS-1:0] a,
+    input logic [DATA_W-1:0] wd,
+    output logic [DATA_W-1:0] rd
+);
+
     logic [DATA_W-1:0] mem [(2**DM_ADDRESS)-1:0];
-    
-    always_comb 
-    begin
-       if (MemRead)
+
+    integer i;
+
+    // Initialize data memory
+    initial begin
+        for (i = 0; i < (2**DM_ADDRESS); i = i + 1)
+            mem[i] = '0;
+    end
+
+    // Asynchronous read
+    always_comb begin
+        if (MemRead)
             rd = mem[a];
-       else
-            rd = '0; // Default value when not reading
+        else
+            rd = '0;
     end
-    
+
+    // Synchronous write
     always @(posedge clk) begin
-       if (MemWrite)
-            mem[a] <= wd; // Use non-blocking assignment for memory write
+        if (MemWrite)
+            mem[a] <= wd;
     end
-    
+
 endmodule
